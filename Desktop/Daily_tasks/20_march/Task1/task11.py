cb95cd6d857a5983111242167b63e97b9dd8aa70
ravikumar-qait <ravikumar@qainfotech.com>
2020-03-23 11:35:50 +0530

    task 1 to convert pdf to json and task2 to edit a pdf

---
+++ Desktop/Daily_tasks/20_march/Task1/task11.py
@@ -0,0 +1,52 @@
+import io
+import json
+from pdfminer.converter import TextConverter
+from pdfminer.pdfinterp import PDFPageInterpreter
+from pdfminer.pdfinterp import PDFResourceManager
+from pdfminer.pdfpage import PDFPage
+
+def extract_text_from_pdf(pdf_path,page_count):
+    resource_manager = PDFResourceManager()
+    fake_file_handle = io.StringIO()
+    converter = TextConverter(resource_manager, fake_file_handle)
+    page_interpreter = PDFPageInterpreter(resource_manager, converter)
+    
+    with open(pdf_path, 'rb') as fh:
+        for page in PDFPage.get_pages(fh, 
+                                      caching=True,
+                                      check_extractable=True):
+            page_interpreter.process_page(page)
+            page_count = page_count+1
+            
+        text = fake_file_handle.getvalue()
+    
+    # close open handles
+    converter.close()
+    fake_file_handle.close()
+    
+    if text:
+        return  text,page_count
+
+text_format = ""
+
+if __name__ == '__main__':
+    text_format,page_count = extract_text_from_pdf('Enrollment_Form_filled.pdf',0)
+
+
+my_dict = {}
+stri = ""
+for i in text_format:
+    if i ==':':
+        if stri=='StudentEnrollmentFormName':
+            stri = 'Name'
+        my_dict[stri]  = ''
+        stri = ""
+    elif i.isalnum():
+        stri = stri+i
+
+json_dict = {}
+json_dict["document.fieldcount"] = len(my_dict)
+json_dict["document.pagecount"] = page_count
+
+json_dict.update(my_dict)
+print(json.dumps(json_dict))
